a706c10ba56e2dc6657e577e5f548018b0ff3955
Reshu-Agarwal-au17/Rishav Agarwal <rishav2510246@gmail.com>
2022-02-15 00:37:07 +0530

    completed practice of fundamental part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -284,3 +284,27 @@ if (day === "monday") {
   console.log(`Not a valid day!`);
 }
 */
+
+// statement and expression
+
+//The conditional ternary operator
+
+/*
+const age = 18;
+age >= 18
+  ? console.log(`I like to drink wine😁`)
+  : console.log(`I like to drink water😁`);
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
