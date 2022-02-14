69ea5a5010755d3c37dd14bc7094bdc604133594
Reshu-Agarwal-au17/Rishav Agarwal <rishav2510246@gmail.com>
2022-02-14 23:32:49 +0530

    coding challenge of fundamental part 1

---
+++ 01-Fundamentals-Part-1/starter/cc.js
@@ -0,0 +1,62 @@
+//CC - 1
+
+/*
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let markBmi = markWeight / markHeight ** 2;
+let johnBmi = johnWeight / johnHeight ** 2;
+let mark_bmi = markWeight / (markHeight * markHeight);
+let john_Bmi = johnWeight / (johnHeight * johnHeight);
+console.log(markBmi);
+console.log(johnBmi);
+console.log(mark_bmi);
+console.log(john_Bmi);
+let markHigherBMI = markBmi > johnBmi;
+console.log(markHigherBMI);
+*/
+/*
+let markMass = 95;
+let markHeight = 1.88;
+let johnMass = 85;
+let johnHeight = 1.76;
+let bmiMark = markMass / markHeight ** 2;
+let bmiJohn = johnMass / (johnHeight * johnHeight);
+console.log(bmiMark);
+console.log(bmiJohn);
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI);
+
+// CC - 2
+if (bmiMark > bmiJohn) {
+  console.log(`Mark's BMI ${bmiMark} is higher than john's ${bmiJohn}!`);
+} else {
+  console.log(`John's BMI ${bmiJohn} is higher than Mark's ${bmiMark}!`);
+}
+*/
+
+//CC - 3
+
+/*
+const teamDolphins = 97 + 112 + 101;
+const teamKoalas = 109 + 95 + 106;
+
+const averageTeamDolphin = teamDolphins / 3;
+const averageTeamKoalas = teamKoalas / 3;
+
+if (averageTeamDolphin > averageTeamKoalas && teamDolphins > 100) {
+  console.log(`Team Dolphin Is Winner`);
+} else if (averageTeamKoalas > averageTeamDolphin && teamKoalas > 100) {
+  console.log(`Team Koalas is winner`);
+} else if (
+  averageTeamDolphin &&
+  teamDolphins >= 100 === averageTeamKoalas &&
+  teamKoalas >= 100
+) {
+  console.log(`Match Draw`);
+} else {
+  console.log(`No team Wins The Trophy`);
+}
+*/
